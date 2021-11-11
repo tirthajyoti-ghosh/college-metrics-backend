4cbdbc306449192ba85e5b5802e55e9f6020730a
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-11-11 12:26:24 +0530

    Change /colleges/:id => /colleges/details/:id
    To make way for /colleges/stats

--- routes/colleges.js
+++ routes/colleges.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const colleges = require('../services/colleges');
 
-router.get('/:id', async (req, res) => {
+router.get('/details/:id', async (req, res) => {
     try {
         res.json(await colleges.getCollegeDetails(req.params.id));
     } catch (err) {
