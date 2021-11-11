9a51ba040ae264c8fe9c2fc61f80c065be695f57
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-11-11 14:59:39 +0530

    Complete students service
    - Create GET /students/:id
    - Create getStudentsByCollege and getStudentDetails functions

--- routes/students.js
+++ routes/students.js
@@ -3,11 +3,12 @@ const express = require('express');
 const router = express.Router();
 const students = require('../services/students');
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        res.json(await students.getMultiple());
+        res.json(await students.getStudentDetails(req.params.id));
     } catch (err) {
-        console.error('Error while getting programming languages', err.message);
+        res.sendStatus(500);
+        console.error('Error', err.message);
     }
 });
 
