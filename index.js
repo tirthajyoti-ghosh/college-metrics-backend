1280d8819de3cdbd08ecaf82bfaf73928cf2e15d
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-11-13 00:33:04 +0530

    Update GET / endpoint to show API docs

--- index.js
+++ index.js
@@ -17,7 +17,48 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-    res.json({ message: 'ok' });
+    res.json({
+        description: 'Welcome to the API! Below you can find all the routes for this API along with descriptions.',
+        routes: {
+            description: 'There are two primary routes => /colleges and /students',
+            colleges: {
+                '/colleges/:id': {
+                    description: 'Returns a single college based on the id provided.',
+                    example: 'https://college-metrics-backend.vercel.app/colleges/618b84b2399bb68fbb64fe56',
+                },
+                '/colleges/stats': {
+                    description: 'Returns the stats for all the colleges i.e., no. of colleges grouped by countries and courses they offer',
+                    example: 'https://college-metrics-backend.vercel.app/colleges/stats',
+                },
+                '/colleges/:id/similar': {
+                    description: 'Returns the similar colleges based on the id provided. Similar in courses offered and location',
+                    example: 'https://college-metrics-backend.vercel.app/colleges/618b84b2399bb68fbb64fe56/similar',
+                },
+                '/colleges/:id/students': {
+                    description: 'Returns the students of a college based on the id provided.',
+                    example: 'https://college-metrics-backend.vercel.app/colleges/618b84b2399bb68fbb64fe56/students',
+                },
+                '/colleges/list': {
+                    description: "Returns a list of all the colleges. If the query params 'type' and 'value' are provided, then only those specific colleges (corresponding to type and value) will be returned. In order to get specific colleges, both 'type' and 'value' query params MUST be provided, otherwise a 400 status code is returned. 'type' query param MUST have one of these two values => 'country' OR 'course'. The value must be in accordance with the type.",
+                    examples: [
+                        'https://college-metrics-backend.vercel.app/colleges/list?type=country&value=United%20States',
+                        'https://college-metrics-backend.vercel.app/colleges/list?type=course&value=Computer%20Science',
+                        'https://college-metrics-backend.vercel.app/colleges/list',
+                    ],
+                    'query-params': {
+                        type: "MUST be 'country' OR 'course'. If this is present, then 'value' param MUST be present.",
+                        value: "MUST be in accordance with the 'type' param. If the 'type' is 'country', then a country should be provided. Putting a course instead will return 404 status code. Same for course. If this is present, then 'type' param MUST be present",
+                    },
+                },
+            },
+            students: {
+                '/students/:id': {
+                    description: 'Returns a single student based on the id provided.',
+                    example: 'https://college-metrics-backend.vercel.app/students/618b9fd92aa253a25e0ef547',
+                },
+            },
+        },
+    });
 });
 
 app.use('/colleges', collegeRouter);
